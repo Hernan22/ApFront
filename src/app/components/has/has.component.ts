ff777dd32fd5a53a5fa79aa427e0d4353eb93bdc
Hernan22 <hernan.chaddo@gmail.com>
2022-07-31 12:36:35 -0300

    arregloPostErrorFatal

---
+++ src/app/components/has/has.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-has',
+  templateUrl: './has.component.html',
+  styleUrls: ['./has.component.scss']
+})
+export class HasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
